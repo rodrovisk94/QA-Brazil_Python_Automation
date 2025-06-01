275ecf70199faf32bc802191b01ed65d06db4a7a
rodrovisk94 <rodrovisk@gmail.com>
2025-06-01 09:55:27 -0300

    commit done

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-9ecaa627-84b1-4fc9-9bb3-6adba8dad2fd.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = 	'+1 123 123 12 12'
--- main.py
+++ main.py
@@ -2,42 +2,51 @@ import data
 import helpers
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
     def test_set_route(self):
-        #Adicionar em S8
+        # Adicionar em S8
         print("função criada para definir a rota")
         pass
 
     def test_select_plan(self):
-        #Adicionar em S8
+        # Adicionar em S8
         print("função criada para selecionar o plano")
         pass
 
     def test_fill_phone_number(self):
-        #Adicionar em S8
+        # Adicionar em S8
         print("função criada para preencher o número de telefone")
         pass
 
     def test_fill_card(self):
-        #Adicionar em S8
+        # Adicionar em S8
         print("função criada para preencher os dados do cartão")
         pass
 
     def test_comment_for_driver(self):
-        #Adicionar em S8
+        # Adicionar em S8
         print("função criada para adicionar comentário para o motorista")
         pass
 
     def test_order_blanket_and_handkerchiefs(self):
-        #Adicionar em S8
+        # Adicionar em S8
         print("função criada para pedir cobertor e lenços")
         pass
 
     def test_order_2_ice_creams(self):
-        #Adicionar em S8
-        print("função criada para pedir 2 sorvetes")
-        pass
+        # Adicionar em S8
+        number_of_ice_creams = 2
+        for count in range(number_of_ice_creams):
+            print("função criada para definir o pedido do sorvete")
+            pass
 
     def test_car_search_model_appears(self):
-        #Adicionar em S8
+        # Adicionar em S8
         print("função criada para verificar se o modelo de busca de carro aparece")
         pass
